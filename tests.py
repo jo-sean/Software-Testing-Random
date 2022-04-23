c7c61cd7bed2cece39663e4a219c640ea88995e7
jo-sean <joseanmanuel@gmail.com>
2022-04-22 20:57:11 -0700

    Took out assertions

--- tests.py
+++ tests.py
@@ -104,8 +104,8 @@ def gen_credit_num(length, prefix, check_sum):
         return append(prefix_num)
 
 
-def generate_testcases_visa(tests_to_generate=500000):
-    """Creates 100 random unit tests for visa credit card numbers"""
+def generate_testcases_visa(tests_to_generate=100000000):
+    """Creates 100000 random unit tests for visa credit card numbers"""
 
     for i in range(tests_to_generate):
 
@@ -126,9 +126,8 @@ def generate_testcases_visa(tests_to_generate=500000):
         credit_card_validator(pwd)
 
 
-
-def generate_testcases_mc_1(tests_to_generate=500000):
-    """Creates 100 random unit tests for mastercard credit card numbers"""
+def generate_testcases_mc_1(tests_to_generate=100000000):
+    """Creates 100000 random unit tests for mastercard credit card numbers"""
 
     for i in range(tests_to_generate):
 
@@ -150,8 +149,8 @@ def generate_testcases_mc_1(tests_to_generate=500000):
         credit_card_validator(pwd)
 
 
-def generate_testcases_mc_2(tests_to_generate=500000):
-    """Creates 100 random unit tests for mastercard credit card numbers"""
+def generate_testcases_mc_2(tests_to_generate=100000000):
+    """Creates 100000 random unit tests for mastercard credit card numbers"""
 
     for i in range(tests_to_generate):
 
@@ -177,8 +176,8 @@ def generate_testcases_mc_2(tests_to_generate=500000):
         credit_card_validator(pwd)
 
 
-def generate_testcases_amex(tests_to_generate=500000):
-    """Creates 100 random unit tests for amex credit card numbers"""
+def generate_testcases_amex(tests_to_generate=100000000):
+    """Creates 100000 random unit tests for amex credit card numbers"""
 
     for i in range(tests_to_generate):
 
